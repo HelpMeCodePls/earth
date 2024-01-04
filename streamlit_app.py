1305f9c3cf30524d6d1f81833e91e9ac57782d01
HelpMeCodePls <64028614+HelpMeCodePls@users.noreply.github.com>
2024-01-04 00:28:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit as st
+'''
 from llama_index import VectorStoreIndex, ServiceContext, Document
 from llama_index.llms import OpenAI
 import openai
 from llama_index import SimpleDirectoryReader
+'''
 st.title("I guess your team is also using streamlit for your hackathon huh... I'll keep an eye on you...👀")
 
 '''
